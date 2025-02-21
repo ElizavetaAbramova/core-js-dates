559bfd66d598182488a5c715b3497affd790759d
Abramova Elizaveta <a.d.elizaveta@gmail.com>
2025-02-21 19:44:26 +0400

    complete getDayName

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -47,8 +47,35 @@ function getTime(date) {
  * '03 Dec 1995 00:12:00 UTC' => 'Sunday'
  * '2024-01-30T00:00:00.000Z' => 'Tuesday'
  */
-function getDayName(/* date */) {
-  throw new Error('Not implemented');
+function getDayName(date) {
+  const dateObj = new Date(date);
+  let day = '';
+  switch (dateObj.getUTCDay()) {
+    case 0:
+      day = 'Sunday';
+      break;
+    case 1:
+      day = 'Monday';
+      break;
+    case 2:
+      day = 'Tuesday';
+      break;
+    case 3:
+      day = 'Wednesday';
+      break;
+    case 4:
+      day = 'Thursday';
+      break;
+    case 5:
+      day = 'Friday';
+      break;
+    case 6:
+      day = 'Saturday';
+      break;
+    default:
+      return false;
+  }
+  return day;
 }
 
 /**
